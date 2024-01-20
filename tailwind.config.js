0970fd76ce2e5327e6419c864e8cbba2b9e87a9c
ShawonECE <send2shawon@gmail.com>
2024-01-20 13:27:17 +0600

    completed till featured section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'carousel': 'rgba(231, 111, 81, 0.10)',
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
